c0c86331441e2771a82afe73e9a20ad003931d63
AbrarMojahidRafi <rafi.cse.bracu@gmail.com>
2024-04-04 17:24:11 +0600

    signin part done and homepage folder added

--- signin.php
+++ signin.php
@@ -1,27 +1,46 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Signin</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	<link href="style.css" rel = "stylesheet">
-  </head>
-  <body>
-    <div class="container">
-      <form action="login.php" method="post">
-        <div class="form-group">
-            <input type="email" placeholder="Enter Email:" name="email" class="form-control">
-        </div>
-        <div class="form-group">
-            <input type="password" placeholder="Enter Password:" name="password" class="form-control">
-        </div>
-        <div class="form-btn">
-            <input type="submit" value="Login" name="login" class="btn btn-primary">
-        </div>
-      </form>
-     <div><p>Don't have an account yet <a href="signup.php">Sign Up</a></p></div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Signin</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+		<link href="style.css" rel = "stylesheet">
+	</head>
+	
+	<body>
+		<div class="container">
+		<?php
+			if (isset($_POST["login"])) {
+				$email = $_POST["email"];
+				$password = $_POST["password"];
+				require_once "databaseConnection.php";
+				$sql_email = "SELECT Email FROM users WHERE Email='$email'"; 
+				$sql_password = "SELECT Password FROM users WHERE Password='$password'";
+				$row_email = mysqli_query($conn, $sql_email); 
+				$row_password = mysqli_query($conn, $sql_password); 
+				if ((mysqli_num_rows($row_email) > 0) && (mysqli_num_rows($row_password) > 0)){
+					echo "Email and Password Match";
+					
+				} else{ // Email or Password is not in the database. 
+					echo "<div class='alert alert-danger' role='alert'> Invalid Email or Password </div>";
+				}
+			}
+		?>
+			<form action="signin.php" method="post">
+				<div class="form-group">
+					<input type="email" placeholder="Enter Email:" name="email" class="form-control">
+				</div>
+				<div class="form-group">
+					<input type="password" placeholder="Enter Password:" name="password" class="form-control">
+				</div>
+				<div class="form-btn">
+					<input type="submit" class="btn btn-primary" value="login" name="login">
+				</div>
+			</form>
+			<div>
+				<p>Don't have an account yet <a href="signup.php">Sign Up</a></p>
+			</div>
+		</div>
+	</body>
 </html>
